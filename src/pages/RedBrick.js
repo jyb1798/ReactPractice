39791cd71d0d93ac40f6bf17ea004069ac31ab59
jyb8509 <bestdung8509@gmail.com>
2022-01-26 19:13:11 +0900

    Add : Router 설정 및 페이지 파일 추가

---
+++ src/pages/RedBrick.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const RedBrick = () => {
+  return <>레드브릭입니다</>;
+};
+
+export default RedBrick;
---
+++ src/pages/WireBarly.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const WireBarly = () => {
+  return <>와이어베어리입니다</>;
+};
+
+export default WireBarly;
