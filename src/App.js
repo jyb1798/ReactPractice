62264c3c122a36e1aa4ae68573ea4f159581d31b
jyb8509 <bestdung8509@gmail.com>
2022-02-02 02:27:53 +0900

    Add: 컴포지션

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ExpenseItem from "./components/ExpensesItem/ExpenseItem";
 import { expense } from "./components/ExpensesItem/Expense";
+import "./components/ExpensesItem/Expense.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="expenses">
       <h2>Let's Get Started!</h2>
       {expense.map((e) => (
         <ExpenseItem
