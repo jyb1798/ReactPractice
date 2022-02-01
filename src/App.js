263514cc5633b567cbc61e0fd33e5983c654a81a
jyb8509 <bestdung8509@gmail.com>
2022-02-02 00:03:13 +0900

    Add: 리팩토링 map

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
 import React from "react";
 import ExpenseItem from "./components/ExpensesItem/ExpenseItem";
+import { expense } from "./components/ExpensesItem/Expense";
 
 const App = () => {
-  const expense = [
-    {
-      id: "ed1",
-      amount: 294.67,
-      title: "Car Insurance",
-      date: new Date(2022, 2, 1),
-    },
-    {
-      id: "ed2",
-      amount: 1000.67,
-      title: "House Insurance",
-      date: new Date(2022, 1, 31),
-    },
-    {
-      id: "ed3",
-      amount: 94.57,
-      title: "Toilet Paper",
-      date: new Date(2022, 1, 15),
-    },
-    {
-      id: "ed4",
-      amount: 11.67,
-      title: "New TV",
-      date: new Date(2022, 2, 1),
-    },
-  ];
   return (
     <div>
       <h2>Let's Get Started!</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
+      {expense.map((e) => (
+        <ExpenseItem
+          key={e.id}
+          title={e.title}
+          amount={e.amount}
+          date={e.date}
+        />
+      ))}
     </div>
   );
 };
---
+++ src/components/ExpensesItem/Expense.js
@@ -0,0 +1,26 @@
+export const expense = [
+  {
+    id: "ed1",
+    amount: 294.67,
+    title: "Car Insurance",
+    date: new Date(2022, 2, 1),
+  },
+  {
+    id: "ed2",
+    amount: 1000.67,
+    title: "House Insurance",
+    date: new Date(2022, 1, 31),
+  },
+  {
+    id: "ed3",
+    amount: 94.57,
+    title: "Toilet Paper",
+    date: new Date(2022, 1, 15),
+  },
+  {
+    id: "ed4",
+    amount: 11.67,
+    title: "New TV",
+    date: new Date(2022, 2, 1),
+  },
+];
