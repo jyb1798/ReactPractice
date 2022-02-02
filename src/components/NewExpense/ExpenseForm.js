d144f6524d1c3043c68578efd42c3c58404bf13e
jyb8509 <bestdung8509@gmail.com>
2022-02-02 21:48:14 +0900

    Add : input status console에 저장 후, 초기화

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -43,6 +43,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredAmount("");
+    setEnteredDate("");
+    setEnteredTitle("");
   };
 
   return (
@@ -50,7 +53,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={changeTitleHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={changeTitleHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -58,6 +65,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={changeAmountHandler}
           />
         </div>
@@ -67,6 +75,7 @@ const ExpenseForm = () => {
             type="date"
             min="2021-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={changeDateHandler}
           />
         </div>
