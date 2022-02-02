98b40b5de5ff06e60212e4245122bfee27741dbf
jyb8509 <bestdung8509@gmail.com>
2022-02-02 20:56:46 +0900

    Add : input status 관리

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const changeHandler = (e) => {
-    return console.log(e.target.value);
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+
+  const changeTitleHandler = (e) => {
+    setUserInput((event) => {
+      return { ...event, enteredTitle: e.target.value };
+    });
   };
+
+  const changeAmountHandler = (e) => {
+    setUserInput({
+      ...userInput,
+      enteredAmount: e.target.value,
+    });
+  };
+
+  const changeDateHandler = (e) => {
+    setUserInput({
+      ...userInput,
+      enteredDate: e.target.value,
+    });
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={changeHandler} />
+          <input type="text" onChange={changeTitleHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={changeAmountHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2021-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2021-01-01"
+            max="2022-12-31"
+            onChange={changeDateHandler}
+          />
         </div>
       </div>
     </form>
