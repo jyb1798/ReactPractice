b42929a7dba2744c5533102b43c9e09aa5023b3a
jyb8509 <bestdung8509@gmail.com>
2022-02-02 19:11:21 +0900

    Add : e.target.value

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const changeHandler = (e) => {
+    return console.log(e.target.value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={changeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -15,11 +18,8 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2021-01-01" step="2022-12-31" />
+          <input type="date" min="2021-01-01" max="2022-12-31" />
         </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
