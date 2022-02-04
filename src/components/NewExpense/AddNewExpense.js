4b8add8143f650e83ae0e0a7034b43a571d15962
jyb8509 <bestdung8509@gmail.com>
2022-02-04 16:56:44 +0900

    Add: 조건부 렌더링 로직 추가, state, true

---
+++ src/components/NewExpense/AddNewExpense.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ExpenseForm.css";
+
+const AddNewExpense = (props) => {
+  return (
+    <button type="submit" value="Add New Expense">
+      Add New Expense
+    </button>
+  );
+};
+
+export default AddNewExpense;
